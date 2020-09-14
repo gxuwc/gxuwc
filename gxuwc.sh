6082c10bc32d4e892ee7315c4428386c042baf70
hackzhu <3110497917@qq.com>
2020-09-14 11:57:33 +0800

    del hostname -I

--- gxuwc.sh
+++ gxuwc.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#IP=$(ip -4 a|grep inet|grep -v 127.0.0.1|cut -d "/" -f1|awk '{print $2}')
+IP=$(ip -4 a|grep inet|grep -v 127.0.0.1|cut -d "/" -f1|awk '{print $2}')
 #IP=$(ifconfig |grep inet|grep -v inet6|grep -v 127|awk '{print $2}')
-IP=$(hostname -I)
 
 ID=""					#学号
 
