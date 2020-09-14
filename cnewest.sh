566e2a1607ba1f4ee819be56df17750f7dcfac0a
hackzhu <3110497917@qq.com>
2020-09-14 10:38:31 +0800

    *.c

--- cnewest.sh
+++ cnewest.sh
@@ -2,7 +2,7 @@
 
 #该脚本可用于学习c语言的过程中，vim保存文件后使用:! command中，无需退出vim即可编译当前c文件
 
-FILE=$(ls -c|head -1|cut -d . -f1)  
+FILE=$(ls -c *.c|head -1|cut -d . -f1)  
 
 gcc -o ${FILE}.exe ${FILE}.c
 ./${FILE}.exe
