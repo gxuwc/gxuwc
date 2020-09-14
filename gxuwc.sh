1705a1bfd2f85aa6a474c687d3a8314d15b8a979
hackzhu <3110497917@qq.com>
2020-09-14 10:19:41 +0800

    增加获取ip的方式

--- gxuwc.sh
+++ gxuwc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-IP=$(ip -4 a|grep inet|grep -v 127.0.0.1|cut -d "/" -f1|awk '{print $2}')
+#IP=$(ip -4 a|grep inet|grep -v 127.0.0.1|cut -d "/" -f1|awk '{print $2}')
+IP=$(hostname -I)
 
 ID=""					#学号
 
