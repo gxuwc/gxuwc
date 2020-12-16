bbfad73672ba107e054c47d0730b22fa3155eea1
hackzhu <3110497917@qq.com>
2020-12-16 19:26:53 +0800

    精简

--- gxuwc.sh
+++ gxuwc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#在gxuwc.sh内输入入学号，密码及运营商。 保证gxuwc.sh有执行权限，直接执行即可
+
+#mac用户可能需要改一下IP地址的获取方式
+
 IP=$(ip -4 a|grep inet|grep -v 127.0.0.1|cut -d "/" -f1|awk '{print $2}')
 #IP=$(ifconfig |grep inet|grep -v inet6|grep -v 127|awk '{print $2}')
 
@@ -11,8 +15,8 @@ PASSWORD=""			#密码	有可能需要转化下URL特殊字符
 WEB_PROVIDER="cmcc"		#移动
 	 #"telecom" 		#电信		
    	 #"unicom"		#联通
-	 #校园网似乎是留空的
+	 #校园网留空,双引号保留
 
-GET="c=ACSetting&a=Login&loginMethod=1&protocol=http%3A&hostname=172.17.0.2&port=&iTermType=1&wlanuserip=${IP}&wlanacip=210.36.18.65&wlanacname=ME60-1&redirect=null&session=null&vlanid=0&mac=06-6f-bb-d0-bf-cd&ip=${IP}&enAdvert=0&jsVersion=2.4.3&DDDDD=%2C0%2C${ID}%40${WEB_PROVIDER}&upass=${PASSWORD}&R1=0&R2=0&R3=0&R6=0&para=00&0MKKey=123456&buttonClicked=&redirect_url=&err_flag=&username=&password=&user=&cmd=&Login="
+GET="c=ACSetting&a=Login&wlanacip=210.36.18.65&ip=${IP}&DDDDD=${ID}%40${WEB_PROVIDER}&upass=${PASSWORD}"
 
 curl http://172.17.0.2:801/eportal/?${GET} --insecure
