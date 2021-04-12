d53bf54a01cd48ae430ddc4bb68e2aa0d8218cc8
hackzhu <3110497917@qq.com>
2021-04-12 21:31:19 +0800

    del ip

--- gxuwc.bat
+++ gxuwc.bat
@@ -3,23 +3,10 @@ set ID=
 
 set PASSWORD=
 
-set WEB_PROVIDER=cmcc
+set WEB_PROVIDER=
 rem	移动	cmcc
 rem	电信	telecom 
 rem	联通	unicom
 rem	校园网	似乎留空
 
-SET IP_FILE=
-ipconfig /all|FINDSTR "IPv4">ipv4.txt
-SETLOCAL EnableDelayedExpansion
-FOR /F "delims=" %%a IN (ipv4.txt) DO (
-    SET IP_FILE=%%a
-    ECHO !IP_FILE:~37,-5!>>ip.txt
-)
-ENDLOCAL
-
-for /f "delims=" %%i in ('type ip.txt') do set IP=%%i
-del ipv4.txt
-del ip.txt
-
-curl "http://172.17.0.2:801/eportal/?c=ACSetting&a=Login&loginMethod=1&protocol=http%%3A&hostname=172.17.0.2&port=&iTermType=1&wlanuserip=%IP%&wlanacip=210.36.18.65&wlanacname=ME60-1&redirect=null&session=null&vlanid=0&mac=06-6f-bb-d0-bf-cd&ip=%IP%&enAdvert=0&jsVersion=2.4.3&DDDDD=%%2C0%%2C%ID%%%40%WEB_PROVIDER%&upass=%PASSwORD%&R1=0&R2=0&R3=0&R6=0&para=00&0MKKey=123456&buttonClicked=&redirect_url=&err_flag=&username=&password=&user=&cmd=&Login="
+curl "http://172.17.0.2:801/eportal/?c=ACSetting&a=Login&wlanacip=210.36.18.65&DDDDD=%ID%@%WEB_PROVIDER%&upass=%PASSWORD%"
--- gxuwc.sh
+++ gxuwc.sh
@@ -4,16 +4,12 @@ ID="$1"           #学号
 
 PASSWORD="$2"     #密码
 
-CARD="$3"         #网卡名
-
-WEB_PROVIDER="$4"
+WEB_PROVIDER="$3"
 			#"cmcc"         #移动
         	#"telecom"      #电信
      	    #"unicom"       #联通
          	#"" 			#校园网
 
-IP=$(ip -4 a show ${CARD} | awk '/inet / {print $2}' | cut -d/ -f1 | grep -v 127.0.0.1 | tail -n1)
-
-GET="c=ACSetting&a=Login&wlanacip=210.36.18.65&ip=${IP}&DDDDD=${ID}@${WEB_PROVIDER}&upass=${PASSWORD}"
+GET="c=ACSetting&a=Login&wlanacip=210.36.18.65&DDDDD=${ID}@${WEB_PROVIDER}&upass=${PASSWORD}"
 
 curl http://172.17.0.2:801/eportal/?${GET} --insecure
