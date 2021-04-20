de2404dcbaf6021d27554407c50a4e266c5580ce
hackzhu <3110497917@qq.com>
2021-04-20 15:36:56 +0800

    “add chinese support”

--- gxuwc.sh
+++ gxuwc.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ID="$1"           #学号
+ID="$1"  #学号
 
-PASSWORD="$2"     #密码
+KEY="$2" #密码
 
-WEB_PROVIDER="$3"
-			#"cmcc"         #移动
-        	#"telecom"      #电信
-     	    #"unicom"       #联通
-         	#"" 			#校园网
+case $3 in
+	移动) WEB="@cmcc"    ;;
+	电信) WEB="@telecom" ;;
+	联通) WEB="@unicom"  ;;
+esac	
 
-GET="c=ACSetting&a=Login&wlanacip=210.36.18.65&DDDDD=${ID}@${WEB_PROVIDER}&upass=${PASSWORD}"
+GET="c=ACSetting&a=Login&wlanacip=210.36.18.65&DDDDD=${ID}${WEB}&upass=${KEY}"
 
-curl http://172.17.0.2:801/eportal/?${GET} --insecure
+curl http://172.17.0.2:801/eportal/?${GET}
